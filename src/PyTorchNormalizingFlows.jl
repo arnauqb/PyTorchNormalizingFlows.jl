3b5872687c52d0d312932a2aa19c0edb0c02e014
Arnau Quera-Bofarull <arnauq@proton.me>
2025-03-16 15:27:57 -0400

    trying to fail gracefuly

--- src/PyTorchNormalizingFlows.jl
+++ src/PyTorchNormalizingFlows.jl
@@ -12,9 +12,14 @@ const torch = PyNULL()
 const normflows = PyNULL()
 
 function __init__()
-    copy!(torch, pyimport("torch"))
-    copy!(normflows, pyimport("normflows"))
-    @pyinclude(String(@__DIR__) * "/wrapper.py")
+    try
+        copy!(torch, pyimport("torch"))
+        copy!(normflows, pyimport("normflows"))
+        @pyinclude(String(@__DIR__) * "/wrapper.py")
+    catch e
+        @warn "Failed to import PyTorch or normflows"
+        @warn "Please check your Python environment"
+    end
 end
 
 # Write your package code here.
